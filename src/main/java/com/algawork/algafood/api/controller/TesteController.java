156fba3b364e388b36291b3987c181af3556dc2a
LucasPera <lucasperamarchi@hotmail.com>
2022-12-30 00:24:03 -0300

    5.7. Usando as keywords para definir critérios de query methods

--- src/main/java/com/algawork/algafood/api/controller/TesteController.java
+++ src/main/java/com/algawork/algafood/api/controller/TesteController.java
@@ -1,9 +1,16 @@
 package com.algawork.algafood.api.controller;
 
+import com.algawork.algafood.domain.model.Cozinha;
+import com.algawork.algafood.domain.model.Restaurante;
 import com.algawork.algafood.domain.repository.CozinhaRepository;
+import com.algawork.algafood.domain.repository.RestauranteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+import java.util.List;
 
 @RestController
 @RequestMapping("/teste")
@@ -12,9 +19,21 @@ public class TesteController {
     @Autowired
     private CozinhaRepository cozinhaRepository;
 
-//    @GetMapping("/cozinhas/por-nome")
-//    public List<Cozinha> cozinhasPorNome(@RequestParam("nome") String nome) {
-//        return cozinhaRepository.consultarPorNome(nome);
-//    }
+    @Autowired
+    private RestauranteRepository restauranteRepository;
+
+    @GetMapping("/cozinhas/por-nome")
+    public List<Cozinha> cozinhasPorNome(String nome) {
+        return cozinhaRepository.findByNomeContaining(nome);
+    }
+    @GetMapping("/restaurantes/por-taxa-frete")
+    public List<Restaurante> restaurantesPorTaxaFrete(BigDecimal taxaInicial, BigDecimal taxaFinal) {
+        return restauranteRepository.findByTaxaFreteBetween(taxaInicial, taxaFinal);
+    }
+
+    @GetMapping("/restaurantes/por-nome-cozinha-e-taxa-frete")
+    public List<Restaurante> cozinhaPorNomeEId(String nomeCozinha, Long idCozinha) {
+        return restauranteRepository.findByNomeContainingAndCozinhaId(nomeCozinha, idCozinha);
+    }
 
 }
--- src/main/java/com/algawork/algafood/domain/repository/CozinhaRepository.java
+++ src/main/java/com/algawork/algafood/domain/repository/CozinhaRepository.java
@@ -4,10 +4,12 @@ import com.algawork.algafood.domain.model.Cozinha;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CozinhaRepository extends JpaRepository<Cozinha, Long> {
 
-//    List<Cozinha> consultarPorNome(String nome);
+    List<Cozinha> findByNomeContaining(String nome);
 
 }
 
