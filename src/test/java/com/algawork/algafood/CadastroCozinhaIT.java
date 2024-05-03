ffdc4486055414fde6edd1af3e14f06901dbde43
LucasPera <lucasperamarchi@hotmail.com>
2024-05-03 01:35:25 -0300

    10.11. Entendendo o problema da ordem de execução dos testes

--- src/test/java/com/algawork/algafood/CadastroCozinhaIT.java
+++ src/test/java/com/algawork/algafood/CadastroCozinhaIT.java
@@ -2,6 +2,8 @@ package com.algawork.algafood;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
@@ -14,6 +16,13 @@ class CadastroCozinhaIT {
 	@LocalServerPort
 	private int port;
 
+	@BeforeEach
+	public void setUp() {
+		RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+		RestAssured.port = port;
+		RestAssured.basePath = "/cozinhas";
+	}
+
 	@Test
 	public void deveRetornarStatusOkQuandoConsultarCozinhas() {
 
@@ -21,8 +30,6 @@ class CadastroCozinhaIT {
 		RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
 
 		RestAssured.given()
-				.basePath("/cozinhas")
-				.port(port)
 				.accept(ContentType.JSON)
 			.when()
 				.get()
@@ -30,5 +37,26 @@ class CadastroCozinhaIT {
 				.statusCode(HttpStatus.OK.value());
 	}
 
+	@Test
+	public void deveConter4CozinhasQuandoConsultarCOzinhas() {
+		RestAssured.given()
+				.accept(ContentType.JSON)
+				.when()
+				.get()
+				.then()
+				.body("", Matchers.hasSize(4))
+				.body("nome", Matchers.hasItems("Indiana", "Tailandesa"));
+	}
+
+	@Test
+	public void deveRetornarStatus201QuandoCadastrarCozinha() {
+		RestAssured.given()
+				.body("{\"nome\": \"Chinesa\"}")
+				.contentType(ContentType.JSON)
+				.when()
+				.post()
+				.then()
+				.statusCode(HttpStatus.CREATED.value());
+	}
 
 }
