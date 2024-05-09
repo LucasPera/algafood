876300b3463ec7dc14455e3907dd1654b2e1ea29
LucasPera <lucasperamarchi@hotmail.com>
2024-05-09 02:01:42 -0300

    10.14. Limpando e populando o banco de dados de test

--- src/test/java/com/algawork/algafood/CadastroCozinhaIT.java
+++ src/test/java/com/algawork/algafood/CadastroCozinhaIT.java
@@ -1,26 +1,42 @@
 package com.algawork.algafood;
 
+import com.algawork.algafood.domain.model.Cozinha;
+import com.algawork.algafood.domain.repository.CozinhaRepository;
+import com.algawork.algafood.util.DatabaseCleaner;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import org.flywaydb.core.Flyway;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
+import org.springframework.test.context.TestPropertySource;
 
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestPropertySource("/application-test.properties")
 class CadastroCozinhaIT {
 
 	@LocalServerPort
 	private int port;
+
+	@Autowired
+	private DatabaseCleaner databaseCleaner;
+
+	@Autowired
+	private CozinhaRepository cozinhaRepository;
 
 	@BeforeEach
 	public void setUp() {
 		RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
 		RestAssured.port = port;
 		RestAssured.basePath = "/cozinhas";
+
+		databaseCleaner.clearTables();
+		prepararDados();
 	}
 
 	@Test
@@ -57,6 +73,18 @@ class CadastroCozinhaIT {
 				.post()
 				.then()
 				.statusCode(HttpStatus.CREATED.value());
+	}
+
+	private void prepararDados() {
+		Cozinha cozinha1 = new Cozinha();
+		cozinha1.setNome("Tailandesa");
+
+		cozinhaRepository.save(cozinha1);
+
+		Cozinha cozinha2 = new Cozinha();
+		cozinha1.setNome("Americana");
+
+		cozinhaRepository.save(cozinha2);
 	}
 
 }
